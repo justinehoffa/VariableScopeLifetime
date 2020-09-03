439709ae70ee1ac1b777ee4098ca93563caf325e
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-03 08:21:37 -0600

    Add project files.

---
+++ VariableScopeLifetime.vb
@@ -0,0 +1,26 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Binary
+
+'Justine Hoffa
+'RCET0265
+'Fall2020
+'VariableScopeLifetime
+
+
+Module VariableScopeLifetime
+
+    Sub Main()
+        Dim x As Integer
+        Console.WriteLine(x)
+
+        Console.ReadLine()
+
+
+
+
+
+
+    End Sub
+
+End Module
